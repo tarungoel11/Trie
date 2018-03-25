1404c9627fd58daebc6cf203d68d94d03322c925
tarungoel11 <tarungoel11@yahoo.com>
2018-03-25 14:17:25 +0530

    first working version

--- Trie.cpp
+++ Trie.cpp
@@ -6,8 +6,13 @@ struct TrieNode
 {
 	char data;
 	TrieNode *child[26];
-	
-	TrieNode(char c):data(c)
+	bool terminates;
+	static int CharToIndex(char c)
+	{
+		//cout<<"accessing at index "<<int(c - 'a')<<endl;
+		return int(c - 'a');
+	}
+	TrieNode(char c):data(c), terminates(false)
 	{
 		std::fill(child, child + 26, (TrieNode*)NULL);
 	}	
@@ -25,7 +30,17 @@ class Trie
 	
 	insert(char *s, int length)
 	{
-		
+		TrieNode *temp = root;
+		for(int i = 0; i<length;i++)
+		{
+			if(temp->child[TrieNode::CharToIndex(s[i])] == NULL )
+			{
+				temp->child[TrieNode::CharToIndex(s[i])] = new TrieNode(s[i]);
+				//cout<<"creating "<<temp<<endl;
+			}
+			temp = temp->child[TrieNode::CharToIndex(s[i])];
+		}
+		temp->terminates = true;
 	}
 	remove(char *s, int length)
 	{
@@ -33,12 +48,31 @@ class Trie
 	}
 	bool find(char *s, int length)
 	{
-		
+		TrieNode *temp = root;
+		for(int i = 0; i<length;i++)
+		{
+			temp = temp->child[TrieNode::CharToIndex(s[i])];
+			if(temp == NULL )
+			{
+				//cout<<"checking "<<temp<<endl;
+				return false;
+			}
+		}
+		if(temp->terminates == true)
+		{
+			return true;
+		}
+		else
+		{
+			return false;
+		}
 	}
 };
 
 int main()
 {
 	Trie t;
+	t.insert("harry", 5);
+	cout<<t.find("harry", 5);
 }
 
